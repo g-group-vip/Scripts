814a7830c9fe682e367c5282c69f46d12ca72b53
Stanisław <g.group.vip@gmail.com>
2024-07-05 17:57:43 +0200

    update of setup.sh

--- Nodes/Farcaster/setup.sh
+++ Nodes/Farcaster/setup.sh
@@ -9,7 +9,6 @@ echo "Installing of the required software!"
 echo "Встановлення необхідного програмного забезпечення!"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 curl -s https://raw.githubusercontent.com/g-group-vip/Scripts/main/Nodes/General/tools_install.sh | bash 
-curl -s https://raw.githubusercontent.com/g-group-vip/Scripts/main/Nodes/General/docker_install.sh | bash 
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Installing of the Farcaster node!"
 echo "Встановлення ноди Farcaster!"
@@ -51,7 +50,7 @@ echo "http://$(hostname -I | awk '{print $1}'):3000"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "As well you can check the actual logs with help of next command:"
 echo "Також актуальні логи можна перевірити наступною командою:"
-echo "docker logs -f --tail=50 hubble-hubble-1"
+echo "docker logs -f --tail=250 hubble-hubble-1"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 curl -s https://raw.githubusercontent.com/g-group-vip/Scripts/main/Nodes/General/thanks.sh | bash
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
