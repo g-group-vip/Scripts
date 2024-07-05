72198bc12a5d283efe7b4d43af2780d39ee0a2b9
Stanisław <g.group.vip@gmail.com>
2024-07-05 16:18:35 +0200

    tools file correction

--- Nodes/General/tools_install.sh
+++ Nodes/General/tools_install.sh
@@ -4,19 +4,10 @@
 # Оновлення списку пакетів та встановлення необхідних утиліт та інструментів розробки
 sudo apt update && sudo apt install -y \
     curl \  # Tool to transfer data from URLs
-    git \  # Version control system Git
     htop \  # Interactive process viewer
     mc \  # Midnight Commander file manager
     wget \  # Utility to download files from the internet
     make \  # Utility for automation of compilation
     cmake \  # Build system
-    clang \  # LLVM compiler
-    netcat \  # Universal network tool
-    net-tools \  # Networking utilities
-    tmux \  # Terminal multiplexer
-    pkg-config \  # Tool for configuring compilation
-    libssl-dev \  # OpenSSL library (header files)
     unzip \  # Utility to unzip zip archives
     jq \  # Tool for processing JSON data
-    build-essential \  # Basic tools required for compilation
-    ncdu \  # Disk analyzer similar to du
